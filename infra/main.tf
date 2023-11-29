ca6fcb862dd634c72adcceb01e707e1ceebe3dbb
= <=>
2023-11-29 13:39:03 -0700

    Commented and cleaned up

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,20 @@
-
+# Required by GKE
 data "google_client_config" "provider" {}
 
-## SETUP KUBERNETES
-##BUCKET FOR STATE
 
-
+# This module is designed to set up the GKE Engine *after* the infrastructure has been set up.
+# We could of used another module to build the project with the roles, but I felt that was out of scope for this. 
+# See: https://github.com/terraform-google-modules/terraform-google-project-factory/tree/master 
 
 module "gke" {
   source  = "terraform-google-modules/kubernetes-engine/google"
   version = "29.0.0"
 
 
-  # insert the 6 required variables here
+  # This should be used as a variable instead of hard-coded
   name = "liatrio-gke-cluster"
 
-  #GCP Project ID
+  # Project_ID Is the Google Cloud Project Id
   project_id = var.project_id
   region     = var.region
 
@@ -33,6 +33,7 @@ module "gke" {
   horizontal_pod_autoscaling = true
   node_pools = [
     {
+      # This should probably be a variable too.
       name               = "liatrio-gke-node-pool"
       min_count          = 1
       max_count          = 2
@@ -48,6 +49,7 @@ module "gke" {
       "https://www.googleapis.com/auth/monitoring",
     ]
   }
+  # Clear any data that may accidentally be persisted
   node_pools_metadata = {
     liatrio-gke-node-pool = {
       shutdown-script = "kubectl --kubeconfig=/var/lib/kubelet/kubeconfig drain --force=true --ignore-daemonsets=true --delete-local-data \"$HOSTNAME\""
@@ -55,7 +57,7 @@ module "gke" {
   }
   node_pools_labels = {
     all = {}
-    pool-01 = {
+    liatrio-gke-node-pool = {
       liatrio-pool-1 = true
     }
   }
--- infra/provider.tf
+++ infra/provider.tf
@@ -1,9 +1,9 @@
-
+# Setup Providers for GCP
 provider "google" {
   project = var.project_id
   region  = var.region
 }
-
+# Set up a kubernetes provider. This is getting populated from the GKE endpoint.
 provider "kubernetes" {
   host                   = "https://${module.gke.endpoint}"
   token                  = data.google_client_config.provider.access_token
