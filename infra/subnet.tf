019186089a2b5108c23e2b2fc92d2920cfbe9d60
= <=>
2023-12-04 11:59:43 -0700

    Removed verison

--- infra/subnet.tf
+++ infra/subnet.tf
@@ -1,13 +1,13 @@
 # Create the subnetwork within the VPC, there are two IP ranges, one for pods, one for services
 resource "google_compute_subnetwork" "private" {
-  name                     = var.subnetwork
-  ip_cidr_range            = "10.0.0.0/18"
-  region                   = var.region
-  network                  = google_compute_network.liatrio-vpc.id
-  private_ip_google_access = true
-  depends_on               = [google_compute_network.liatrio-vpc]
+  name                       = var.subnetwork
+  ip_cidr_range              = "10.0.0.0/18"
+  region                     = var.region
+  network                    = google_compute_network.liatrio-vpc.id
+  private_ip_google_access   = true
+  depends_on                 = [google_compute_network.liatrio-vpc]
   private_ipv6_google_access = "ENABLE_BIDIRECTIONAL_ACCESS_TO_GOOGLE"
- log_config {
+  log_config {
     aggregation_interval = "INTERVAL_5_SEC"
     flow_sampling        = 0.5
     metadata             = "INCLUDE_ALL_METADATA"
